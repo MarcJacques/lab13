82d0579ca8b959c3c983954e8485cbe89fc14548
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-02-18 14:26:35 +0000

    adds for loop to enter information adds for loops to return snow fall report

--- lab13.cpp
+++ lab13.cpp
@@ -1,5 +1,48 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main() 
+{
+  const int SIZE = 7;
+  int date[SIZE];
+  double snowHeight[SIZE];
+  string month;
+  int peakSnowDay = 0;
+  double totalSnow = 0;
+  double average;
+
+  cout << "Enter the name of the month: " << endl;
+  cin >> month;
+
+  for(int i=0; i < SIZE; i++)
+     {
+       cout << "Enter the date " <<i+1<<endl;
+       cin >> date[i];
+
+       cout << "Enter number of inches of snow for day" <<i+1<<endl;
+       cin >> snowHeight[i];
+     }
+  
+  cout << "Snow report "<<month<<" "<< date[0]<<" - "<< date[SIZE - 1]<< endl;
+  cout << "Date      Snow Fall" << endl;
+  
+  for(int i=0; i < SIZE; i++)
+    {
+      if(i > 0)
+      {
+        if(snowHeight[peakSnowDay] < snowHeight[i])
+        peakSnowDay = i;
+      }
+
+      totalSnow += snowHeight[i];
+      
+      cout << date[i]<<"         "<<snowHeight[i]<< endl;
+      
+    }
+  average = totalSnow/SIZE;
+
+  cout <<"Height snow fall is " <<snowHeight[peakSnowDay]<<" on "<<date[peakSnowDay]<<"th and the average snow fall is "<<average<<endl;
+  
+  return 0;
 }
